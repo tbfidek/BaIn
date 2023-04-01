e6aaf303dc2546b931013cb19bbf446ccfccf6cf
daria <dariae9@yahoo.com>
2023-04-01 17:12:34 +0300

    action buttons work + made the welcome section

--- main_page.js
+++ main_page.js
@@ -20,6 +20,94 @@ function addNewSection() {
     document.getElementById("babyID").appendChild(newSection);
 }
 
+function showJournal() {
+    const journal = document.querySelector('.BABY-PROFILE');
+    journal.style.display = 'block';
+    let others = document.querySelector('.SLEEPING-SCHEDULE');
+    others.style.display = 'none';
+    others = document.querySelector('.FEEDING-TIME');
+    others.style.display = 'none';
+    others = document.querySelector('.MEDICAL-HISTORY');
+    others.style.display = 'none';
+    others = document.querySelector('.GALLERY');
+    others.style.display = 'none';
+    others = document.querySelector('.EDIT-CHILD');
+    others.style.display = 'none';
+}
+function showSleepingSchedule() {
+    const sleepingSchedule = document.querySelector('.SLEEPING-SCHEDULE');
+    sleepingSchedule.style.display = 'block';
+    let others = document.querySelector('.BABY-PROFILE');
+    others.style.display = 'none';
+    others = document.querySelector('.FEEDING-TIME');
+    others.style.display = 'none';
+    others = document.querySelector('.MEDICAL-HISTORY');
+    others.style.display = 'none';
+    others = document.querySelector('.GALLERY');
+    others.style.display = 'none';
+    others = document.querySelector('.EDIT-CHILD');
+    others.style.display = 'none';
+}
+
+function showFeedingTime() {
+    const feedingTime = document.querySelector('.FEEDING-TIME');
+    feedingTime.style.display = 'block';
+    let others = document.querySelector('.BABY-PROFILE');
+    others.style.display = 'none';
+    others = document.querySelector('.SLEEPING-SCHEDULE');
+    others.style.display = 'none';
+    others = document.querySelector('.MEDICAL-HISTORY');
+    others.style.display = 'none';
+    others = document.querySelector('.GALLERY');
+    others.style.display = 'none';
+    others = document.querySelector('.EDIT-CHILD');
+    others.style.display = 'none';
+}
+
+function showMedicalHistory() {
+    const medicalHistory = document.querySelector('.MEDICAL-HISTORY');
+    medicalHistory.style.display = 'block';
+    let others = document.querySelector('.BABY-PROFILE');
+    others.style.display = 'none';
+    others = document.querySelector('.SLEEPING-SCHEDULE');
+    others.style.display = 'none';
+    others = document.querySelector('.FEEDING-TIME');
+    others.style.display = 'none';
+    others = document.querySelector('.GALLERY');
+    others.style.display = 'none';
+    others = document.querySelector('.EDIT-CHILD');
+    others.style.display = 'none';
+}
+
+function showGallery() {
+    const gallery = document.querySelector('.GALLERY');
+    gallery.style.display = 'block';
+    let others = document.querySelector('.BABY-PROFILE');
+    others.style.display = 'none';
+    others = document.querySelector('.SLEEPING-SCHEDULE');
+    others.style.display = 'none';
+    others = document.querySelector('.FEEDING-TIME');
+    others.style.display = 'none';
+    others = document.querySelector('.MEDICAL-HISTORY');
+    others.style.display = 'none';
+    others = document.querySelector('.EDIT-CHILD');
+    others.style.display = 'none';
+}
+
+function showEditChildProfile() {
+    const childProfile = document.querySelector('.EDIT-CHILD');
+    childProfile.style.display = 'block';
+    let others = document.querySelector('.BABY-PROFILE');
+    others.style.display = 'none';
+    others = document.querySelector('.SLEEPING-SCHEDULE');
+    others.style.display = 'none';
+    others = document.querySelector('.FEEDING-TIME');
+    others.style.display = 'none';
+    others = document.querySelector('.MEDICAL-HISTORY');
+    others.style.display = 'none';
+    others = document.querySelector('.GALLERY');
+    others.style.display = 'none';
+}
 
 
 
