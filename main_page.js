490f2519918c0734d758703eb1b27d62e3fd5b4b
daria <dariae9@yahoo.com>
2023-04-01 15:49:32 +0300

    add + logout button works

--- main_page.js
+++ main_page.js
@@ -1,4 +1,25 @@
 function themeToggle() {
-    var element = document.body;
+    const element = document.body;
     element.classList.toggle("dark-theme");
 }
+
+// dc nu merge :(((
+function removeChild(element) {
+    const section = element.parentElement.parentElement;
+    section.remove();
+}
+
+
+function addNewSection() {
+    const newSection = document.createElement('section');
+
+    const content =`<h2>Baby </h2>
+                            <span onclick='removeChild(this)' class='material-symbols-rounded'>person_remove</span>`;
+    newSection.innerHTML = content;
+
+    document.getElementById("babyID").appendChild(newSection);
+}
+
+
+
+
