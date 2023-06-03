5b81ee1c82b0b86f9af9118ba57d46d72294970a
daria <dariae9@yahoo.com>
2023-06-03 20:52:12 +0300

    404 working

--- src/server.js
+++ src/server.js
@@ -1,13 +1,8 @@
 import * as http from "http";
-import path, {dirname} from "path";
+import path, { dirname } from "path";
 import serveStatic from 'serve-static';
 import { fileURLToPath } from 'url';
-
-// import pkg from 'pg';
-// const { Pool } = pkg;
-// import database from './config.js';
-//
-// const pool = new Pool(database);
+import * as fs from "fs";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = 3000;
@@ -18,18 +13,14 @@ let serve = serveStatic(path.join(__dirname, 'public'), {
 
 const server = http.createServer((req, res) => {
     serve(req, res, () => {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(fs.readFileSync(path.join(__dirname, 'public/views', 'errorPage.html'), 'utf8'));
     });
-    // pool.connect((err, client, release) => {
-    //     if (err) throw err;
-    //     console.log('Connected!');
-    //
-    //     // Do some database operations here...
-    //
-    //     release();
-    //     console.log('Connection released!');
-    // });
+
+
     const url = req.url;
-    if(url.match(/main/)){
+    if (url.match(/main/)) {
     }
 });
 
@@ -38,7 +29,6 @@ server.listen(port, () => {
 });
 
 process.on('SIGINT', function () {
-    // db.close();
     server.close();
     process.exit();
 });
