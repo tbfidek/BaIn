8ae1dd747a7ad0747e3b262a5b218759bc5b27b3
daria <dariae9@yahoo.com>
2023-06-11 21:36:59 +0300

    logout from edit profile works

--- src/public/scripts/editProfileFunctions.js
+++ src/public/scripts/editProfileFunctions.js
@@ -120,3 +120,22 @@ function updatePassword() {
 
 window.onload = populateInfo;
 
+function logout() {
+    fetch('/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+                window.location.href = "http://localhost:3000/views/login.html";
+            } else {
+                alert('An error occurred while logging out.');
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            alert('An error occurred while logging out.');
+        });
+}
