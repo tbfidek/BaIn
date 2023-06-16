29cabf251eb9a616affbd673e2f8e258e9d9554c
Ioana Peste <impeste@gmail.com>
2023-06-16 12:20:54 +0300

    fixed edit child profile bug

--- src/database.js
+++ src/database.js
@@ -2,7 +2,13 @@ import pkg from 'pg';
 const { Pool } = pkg;
 import database from './config.js';
 const pool = new Pool(database);
+pkg.types.setTypeParser(1114, function (value) {
+    return value
+})
 
+pkg.types.setTypeParser(1082, function(value) {
+    return value;
+});
 pool.connect((err, client, release) => {
     if (err) throw err;
     console.log('Connected!');
